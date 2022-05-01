758b9436d9cd75fbc6dfbe0fecbb57e6126d84fb
MayMhuu <maymyatmon290@gmail.com>
2022-05-02 01:40:45 +0630

    fixed 404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import WeatherForecast from "./views/WeatherForecast";
 
 function App() {
   return (
     <HashRouter>
-      <WeatherForecast />
+      <Route exact path="/" element={<WeatherForecast />} />
+
     </HashRouter>
   );
 }
